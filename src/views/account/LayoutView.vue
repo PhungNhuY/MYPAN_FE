6c030c2d75c50f0e2093acd6168014139e754394
PhungNhuY <yphung282@gmail.com>
2023-03-23 22:47:01 +0700

    add navbar

---
+++ src/views/account/LayoutView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import router from '@/router';
+import { useAuthStore } from '@/stores/auth.store';
+
+// redirect home if already logged in
+const authStore = useAuthStore();
+if(authStore.user){
+    router.push('/');
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 offset-sm-2 mt-5">
+                <RouterView />
+            </div>
+        </div>
+    </div>
+</template>
