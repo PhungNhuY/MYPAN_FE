c752827e2f4ed75886f8cbae8ddcd61e77105cfb
PhungNhuY <yphung282@gmail.com>
2023-03-29 09:36:03 +0700

    update vuetify

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,14 @@ import "bootstrap"
 import './assets/main.css'
 
 
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
+// import './styles/index.scss';
 
 const app = createApp(App)
 
@@ -23,6 +27,13 @@ app.use(router)
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
 });
 
 app.use(vuetify);
