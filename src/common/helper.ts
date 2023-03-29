a06275d688880a548f53804392963917e117b1d9
PhungNhuY <yphung282@gmail.com>
2023-03-29 20:11:44 +0700

    add common functions

--- src/common/helper.ts
+++ src/common/helper.ts
@@ -11,3 +11,22 @@ export function showSuccessNotificationFunction(message: string, title?: string)
     const { appendToast } = useToasts();
     appendToast({ message, type: ToastType.SUCCESS, title: title || '' });
 }
+
+export function isValidJSON(str: string) {
+    try {
+        const object = JSON.parse(str);
+        if (object && typeof object === 'object') return true;
+        return false;
+    } catch (error) {
+        return false;
+    }
+}
+
+export function isStringify<T>(obj: T | Record<string, unknown>): boolean {
+    try {
+        JSON.stringify(obj);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
