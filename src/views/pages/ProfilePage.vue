7bca980af672e793405487efbc5bcb844233c487
PhungNhuY <yphung282@gmail.com>
2023-04-05 22:08:26 +0700

    update auth

--- src/views/pages/ProfilePage.vue
+++ src/views/pages/ProfilePage.vue
@@ -1,14 +1,18 @@
-<script setup lang="ts">import httpService, { callApi } from '@/services/http/http.service';
-import authStorageService from '@/services/local-storage/authStorage.service';
+<script setup lang="ts">
+import httpService, { callApi } from '@/services/http/http.service';
+import { useAuthStore } from '@/stores/Auth.store';
+// import authStorageService from '@/services/local-storage/authStorage.service';
 
-const response = await callApi(httpService.get('/user', {
-    headers:{
-        accesstoken: authStorageService.getAccessToken(),
-    }
-}));
-const user = response.data?.user;
+// const response = await callApi(httpService.get('/user', {
+//     headers:{
+//         accesstoken: authStorageService.getAccessToken(),
+//     }
+// }));
+// const user = response.data?.user;
+
+const {user} = useAuthStore();
 </script>
 
 <template>
-    <div class="">{{ user}}</div>
+    <div class="">{{ user }}</div>
 </template>
