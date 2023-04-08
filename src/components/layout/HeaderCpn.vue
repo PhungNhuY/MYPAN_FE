dd8b40570887ce35666e1c619ae1aab68d5ca879
PhungNhuY <yphung282@gmail.com>
2023-04-08 23:04:56 +0700

    header

--- src/components/layout/HeaderCpn.vue
+++ src/components/layout/HeaderCpn.vue
@@ -12,17 +12,85 @@ function searchAction() {
 </script>
 
 <template>
-    <ul class="">
-        <li class="" v-show="authStore.user?.email">
-            <router-link to="/getMe" class="">GetMe</router-link>
-        </li>
-        <li class="" v-show="authStore.user?.email">
-            <button @click="authStore.logout()" class="">Logout</button>
-        </li>
-        <li class="" v-show="!authStore.user?.email">
-            <router-link to="/auth/login" class="" href="#">Login</router-link>
-        </li>
-    </ul>
+    <div class="container-fluid wrapper d-flex align-items-center">
+        <img src="@/assets/images/logo_100_text.png" alt="" srcset="" class="logo">
+        <form class="search-form" action="" method="post">
+            <input type="text" class="search-form-input" name="search" placeholder="Gõ vào tên các nguyên liệu...">
+            <button type="submit" class="search-form-submit">
+                <img src="@/assets/icons/search-interface-symbol.png" alt="" srcset="">
+            </button>
+        </form>
+        <div class="menu">
+            <img src="@/assets/icons/burger-bar.png" alt="" srcset="" class="menu-icon">
+        </div>
+    </div>
+    <!-- <ul class="">
+            <li class="" v-show="authStore.user?.email">
+                <router-link to="/getMe" class="">GetMe</router-link>
+            </li>
+            <li class="" v-show="authStore.user?.email">
+                <button @click="authStore.logout()" class="">Logout</button>
+            </li>
+            <li class="" v-show="!authStore.user?.email">
+                <router-link to="/auth/login" class="" href="#">Login</router-link>
+            </li>
+        </ul> -->
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+p {
+    margin: 0px;
+    padding: 0px;
+}
+
+.wrapper {
+    height: 60px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    border-bottom: 1px solid #DCDCDC;
+}
+
+.logo {
+    height: 40px;
+    width: auto;
+}
+
+.search-form {
+    width: 100%;
+    padding: 0px 8px;
+    position: relative;
+    margin-right: 10px;
+    .search-form-input {
+        width: 100%;
+        height: 36px;
+        border-radius: 99px;
+        border: 1px solid #DCDCDC;
+        padding: 0px 12px;
+        &:focus{
+            outline: none;
+        }
+    }
+    
+    .search-form-submit{
+        position: absolute;
+        height: 36px;
+        width: 50px;
+        top: 0px;
+        right: 0px;
+        background-color: #ffaa55;
+        border-radius: 0px 99px 99px 0px;
+    }
+    
+    .search-form-submit img{
+        width: 20px;
+        height: 20px;
+    }
+}
+
+.menu{
+    .menu-icon{
+        width: 40px;
+        height: 40px;
+    }
+}
+</style>
