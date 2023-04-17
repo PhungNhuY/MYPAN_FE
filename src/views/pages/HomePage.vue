c787ed6c14e15329f2d2d81b3014e86ed5e224f7
PhungNhuY <yphung282@gmail.com>
2023-04-17 23:24:56 +0700

    add prop for postCard

--- src/views/pages/HomePage.vue
+++ src/views/pages/HomePage.vue
@@ -3,6 +3,7 @@ import { useAuthStore } from '@/stores/Auth.store';
 import { storeToRefs } from 'pinia';
 import SliderAndSearch from '@/components/SliderAndSearch.vue';
 import PostCard from '@/components/PostCard.vue';
+import type { IPostCard } from '@/common/interfaces';
 
 const authStore = useAuthStore();
 const {user} = storeToRefs(authStore);
@@ -14,7 +15,16 @@ const {user} = storeToRefs(authStore);
         <div class="row">
             <div class="col-7 offset-1 main-col">
                 <SliderAndSearch/>
-                <PostCard />
+                <PostCard :post-card="{
+                    id: '1234',
+                    imageCover: '/slider-temp/1.webp',
+                    postName: 'Sườn non hảo hạng',
+                    username: 'Phung Nhu Y',
+                    userAvatar: null,
+                    like: 100,
+                    isLike: false,
+                    isFavotire: false,
+                }"/>
             </div>
             <div class="col-3 feature-col">
                 adbe
